165ccdaa5eebad17526fe63b6dd1f816ba54496a
sayaz <saffaayaz@gmail.com>
2023-07-19 17:58:35 +0200

    LWC code updated

--- force-app/main/default/lwc/widgetInputForm/widgetInputForm.js
+++ force-app/main/default/lwc/widgetInputForm/widgetInputForm.js
@@ -5,39 +5,50 @@ import saveWidget from '@salesforce/apex/WidgetController.saveWidget';
 
 export default class WidgetInputForm extends LightningElement {
     @track widgetValue = '';
-    @track disableSaveButton = true;
     @track showValidationMessage = false;
     @track validationMessage = '';
     @track isSystemAdmin = false;
+    @track isWidgetMaster = false;
 
+    // Wire the User's profile name and check if the current user is a System Admin or Widget Master
     @wire(getRecord, { recordId: '$currentUserId', fields: ['User.Profile.Name'] })
-    currentUser;
+    currentUser({ error, data }) {
+        if (data) {
+            const profileName = data.fields.Profile.value.fields.Name.value;
+            this.isSystemAdmin = profileName === 'System Administrator';
+            this.isWidgetMaster = profileName === 'Widget Master';
+        } else if (error) {
+            console.error('Error retrieving user data:', error);
+        }
+    }
 
     get currentUserId() {
         return this.currentUser.data ? this.currentUser.data.fields.Id.value : null;
     }
 
     get isSaveDisabled() {
-        return this.disableSaveButton || (!this.isSystemAdmin && !this.isWidgetValueValid());
+        return (
+            (!this.isSystemAdmin && !this.isWidgetMaster && (this.widgetValue === '' || !this.isWidgetValueValid())) ||
+            (this.isSystemAdmin && !this.isWidgetValueValid())
+        );
     }
 
     handleInputChange(event) {
         this.widgetValue = event.target.value;
-        this.disableSaveButton = !this.isWidgetValueValid();
         this.showValidationMessage = false;
     }
 
     isWidgetValueValid() {
         // Get the widget value
         const value = this.widgetValue.trim();
-    
+
         // Stack to track opening symbols
         const stack = [];
-    
+
         // Iterate through each character in the value
         for (let i = 0; i < value.length; i++) {
             const char = value[i];
-    
+
             if (char === '(' || char === '[' || char === '{') {
                 // Push opening symbols onto the stack
                 stack.push(char);
@@ -46,9 +57,9 @@ export default class WidgetInputForm extends LightningElement {
                 if (stack.length === 0) {
                     return false;
                 }
-    
+
                 const lastOpeningSymbol = stack.pop();
-    
+
                 // Check if the opening and closing symbols match
                 if (
                     (lastOpeningSymbol === '(' && char !== ')') ||
@@ -59,14 +70,13 @@ export default class WidgetInputForm extends LightningElement {
                 }
             }
         }
-    
+
         // Return true if the stack is empty (all opening symbols have been closed)
         return stack.length === 0;
     }
-    
 
     handleSaveWidget() {
-        if (this.isWidgetValueValid() || this.isSystemAdmin) {
+        if (this.isWidgetValueValid()) {
             saveWidget({ widgetValue: this.widgetValue })
                 .then(result => {
                     // Show success toast message
@@ -80,7 +90,7 @@ export default class WidgetInputForm extends LightningElement {
 
                     // Reset the widget value
                     this.widgetValue = '';
-                    this.disableSaveButton = true;
+                    this.showValidationMessage = false;
                 })
                 .catch(error => {
                     // Show error toast message
